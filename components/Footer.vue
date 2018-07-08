fde4308a93a1947a4a461d4bffb600abed200348
Ali Gavahi <aligavahi@yahoo.com>
2018-07-08 21:14:19 +0430

    add footer and header component in default layout

---
+++ components/Footer.vue
@@ -0,0 +1,90 @@
+<head>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<template>        
+        <footer>
+            
+            <div class="big">
+               تمامی حقوق این سایت متعلق به دانشگاه امیر کبیر بوده و هر گونه استفاده از منابع و طراحی های این سایت توسط نهادی خارج از دانشگاه با پیگرد قانونی همرا خواهد بود
+            </div>
+            
+            <div class="small">
+                <ul>
+                    اطلاعات ما
+                    <li>
+                       درباره ما
+                    </li>
+                    <li>
+                       تماس با ما
+                    </li>
+                </ul>
+            </div>
+            
+            <div class="big">
+                <ul>
+                    به ما در رسانه های اجتماعی بپیوندید
+                    <li>
+                        <a href="#" class="fa fa-facebook"></a>
+                        <a href="#" class="fa fa-google-plus"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                    </li>
+                    <li>
+                        <a href="#" class="fa fa-facebook"></a>
+                        <a href="#" class="fa fa-google-plus"></a>
+                        <a href="#" class="fa fa-twitter"></a>
+                    </li>
+                </ul>
+            </div>
+            
+            <div class="small">
+                <ul>
+                    خدمات مشتریان
+                    <li>
+                        سوالات متداول
+                    </li>
+                    <li>
+                        بازگرداندن کالا
+                    </li>
+                    <li>
+                        شرایط استفاده
+                    </li>
+                    <li>
+                        حریم شخصی
+                    </li>
+                </ul>
+            </div>
+            
+        </footer>
+</template> 
+
+<style scoped>
+    footer {
+         display: flex;
+         background-color: gainsboro;
+    }
+    div.big {
+      width: 30%;
+      padding: 3%;
+      align-self: center;
+    }
+
+    div.small {
+      width: 20%;
+      padding: 3%;
+    }
+
+    div{
+        font-size: 1em;
+    }
+
+    ul { 
+         text-align: right;
+         list-style-type: none;
+         font-weight: bold;
+    }
+
+    ul li{
+        font-size: .9em;
+        font-weight: normal;
+    }
+</style>
---
+++ components/Header.vue
@@ -0,0 +1,104 @@
+<template>
+  <header>
+          <div class="upper">
+              <div class="search-container">
+                    <form>
+                        <input type="text" placeholder="Search.." name="search">
+                        <button type="submit"><i class="fa fa-search"></i></button>
+                    </form>
+              </div>
+              <button style="width:auto;">ورود</button>
+              <button style="width:auto;">عضویت</button>
+          </div>
+            
+          <div class="lower">
+            <ul><li>برندها</li><li>تخفیف</li> <li>ورزشی</li><li>بچگانه</li><li>زنانه</li><li>مردانه</li></ul>           
+          </div>
+  </header>
+</template>
+
+
+<style scoped>
+  .lower{
+      align-content: center;
+      border-bottom: 2px solid black;
+      padding-left: 12%;
+      padding-right: 12%;
+      margin-bottom: 6px;
+  }
+
+  ul{
+      padding:0px;
+      list-style-type: none;
+      margin-bottom: 0px;
+  }
+
+  ul li{
+      display: inline;
+      margin-left: 6%;
+      margin-right: 6%;
+      padding: 2px;
+  }
+
+  ul li:hover{
+      color: blue;
+      text-decoration: underline blue;
+      
+  }
+
+  .upper{
+      float: none;
+      overflow: hidden;
+  }
+
+  .search-container {
+    float: left;
+  }
+
+  input[type=text] {
+    padding: 6px;
+    margin-top: 8px;
+    font-size: 17px;
+    border: none;
+  }
+
+  .search-container button {
+    float: left;
+    padding: 6px 10px;
+    margin-top: 8px;
+    margin-right: 16px;
+    background: #ddd;
+    font-size: 17px;
+    border: none;
+    cursor: pointer;
+  }
+
+  @media screen and (max-width: 600px){
+      header input[type=text], header .search-container button {
+          float: none;
+          display: block;
+          text-align: left;
+          width: 100%;
+          margin: 0;
+          padding: 14px;
+      }
+
+      header input[type=text] {
+          border: 1px solid #ccc;  
+      }
+  }
+  .upper > button {
+      float: right;
+      border-radius: 25px;
+      padding: 14px 20px;
+      margin: 8px 0;
+      border: none;
+      cursor: pointer;
+      width: 100%;
+  }
+
+  .upper > button:hover {
+      background-color: darkgray;
+  }
+
+</style>
