66315f7c7fc0239d35d17391c189caaa6da22847
Ali Gavahi <aligavahi@yahoo.com>
2018-07-20 08:42:24 +0430

    add new components to buy page

--- pages/buy/index.vue
+++ pages/buy/index.vue
@@ -1,14 +1,27 @@
 <template>
-  
+	<div style="display: flex;">
+		<div id="left">
+		</div>
+		<div id="right">
+			<SHContainer type = "list" title = "دسته بندی ها"/>
+		</div>
+	</div>
 </template>
 
 <script>
-
+import SHContainer from '@/components/SHContainer'
 export default {
   components: {
+  	SHContainer
   }
 }
 </script>
 
 <style>
+#left{
+	width: 100%;
+}
+#right{
+	width: 20%;
+}
 </style>
