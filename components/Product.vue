3a027a2ed6476c71b4860d7c8d4e23a7c6ce22e1
Ali Gavahi <aligavahi@yahoo.com>
2018-07-20 15:02:36 +0430

    add new product component to products page

---
+++ components/Product.vue
@@ -0,0 +1,94 @@
+<template>
+<span id= "P_cont" :style="'font-size: ' + size + ';background-color: ' + color" @mouseover="emph()" @mouseout="norm()">
+  <nuxt-link to="/detail">
+        <div class="image" :style="'background-image: url('+src+')'"> </div>
+        <div>  {{b_name}} </div>
+        <div class="gray">  {{p_name}} </div>
+        <div> <span class="red"> {{real_price}} </span> <span class="dec"> {{price}} </span> </div>
+        <div class="red"> تومان </div>
+  </nuxt-link>
+  <div class="percentage"><span class="middle">{{off_percent}}</span></div>
+</span>
+</template>
+
+<script >
+
+export default {
+  props:["src","b_name","p_name","price","real_price"],
+  methods: {
+      norm(){
+        this.size = ".75em";
+        this.color = "rgba(100,100,100,.5)";
+      },
+
+      emph(){
+        this.size = ".8em";
+        this.color = "rgba(200,200,200,.5)";
+      }
+  },
+   data() {
+    return {
+      size:".7em",
+      color:"rgba(100,100,100,.5)",
+      off_percent:"10%"
+    }
+  }
+}
+</script>
+
+
+
+<style scoped>
+
+#P_cont {
+  box-sizing: border-box;
+  padding: 3px;
+  margin: 10px;
+  background-color: #888888;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-position: center;
+  width: 20%;
+  height: 250px;
+  position:relative;
+  display: inline-flex;
+  flex-flow: column;
+  text-align: center;
+}
+.gray {
+  color: gray;
+}
+
+.red {
+  color: red;
+}
+
+.dec{
+  text-decoration: line-through;
+}
+
+.image{
+  width: 100%;
+  height: 150px;
+  background-position: center;
+  background-size: cover;
+  background-repeat: no-repeat;
+}
+
+.percentage{
+  position: absolute;
+  right: -15px;
+  top: -15px;
+  border-radius: 50%;
+  width: 50px;
+  height: 50px;
+  background-color: pink;
+  text-align: center;
+  display: table;
+}
+
+.middle{
+display: table-cell;
+vertical-align: middle;
+}
+</style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,6 +1,33 @@
 <template>
-	<div style="display: flex;">
+	<div class="flex">
 		<div id="left">
+			<div >
+				<h5>{{group_name}}</h5>
+				<div class="flex">
+					<div class="flex">
+						<button> صفحه قبل</button>
+						<div> صفحه {{cur_page}} از {{total_page}} </div>
+						<button> صفحه بعد</button>
+					</div> 
+					<div style="margin-left: 50%;"> 
+						<select>
+							<option>محبوبیت</option>
+							<option>قیمت</option>
+							<option>تخفیف</option>
+						</select>
+						مرتب سازی
+					</div>
+				</div>
+			</div>
+			<div id="results">
+				<Product v-for = "product in products"
+				 :b_name = "product.bname"
+				 :p_name = "product.pname"
+				 :src = "product.src"
+				 :price = "product.price"
+				 :real_price = "product.real_price"
+				 />
+			</div>
 		</div>
 		<div id="right">
 			<SHContainer type = "list" title = "دسته بندی ها"/>
@@ -12,18 +39,62 @@
 
 <script>
 import SHContainer from '@/components/SHContainer'
+import Product from '@/components/Product'
 export default {
   components: {
-  	SHContainer
+  	SHContainer,
+  	Product
+  },
+  data(){
+  	return{
+  		group_name:"موبایل",
+  		total_page:5,
+  		cur_page:2,
+  		products : [ {id:1,bname:"apple",pname:"تلفن",src:"",price:100,real_price:50},
+  					 {id:2,bname:"nokia",pname:"تلفن",src:"",price:300,real_price:50},
+  					 {id:3,bname:"samsung",pname:"یخچال",src:"",price:100,real_price:50},
+  					 {id:4,bname:"asus",pname:"تلویزیون",src:"",price:500,real_price:100},
+  					 {id:5,bname:"samsung",pname:"ماشین لباس شویی",src:"",price:100,real_price:50},
+  					 {id:6,bname:"nokia",pname:"کمد",src:"",price:100,real_price:50},
+  					 {id:7,bname:"samsung",pname:"لباس",src:"",price:605,real_price:600},
+  					 {id:8,bname:"nokia",pname:"میز",src:"",price:100,real_price:50},
+  					 {id:9,bname:"apple",pname:"موبایل",src:"",price:100,real_price:50},
+  					 {id:10,bname:"apple",pname:"موبایل",src:"",price:100,real_price:50},
+  					 {id:11,bname:"asus",pname:"تلفن",src:"",price:125,real_price:50},
+  					 {id:12,bname:"asus",pname:"یخچال",src:"",price:100,real_price:50},
+  					 {id:13,bname:"htc",pname:"کمد",src:"",price:250,real_price:230},
+  					 {id:14,bname:"htc",pname:"لباس",src:"",price:120,real_price:100}
+  		]
+  	}
   }
 }
 </script>
 
-<style>
+<style scoped>
 #left{
 	width: 100%;
+	padding: 2%;
+}
+h5{
+	text-align: right;
+	clear: both;
 }
 #right{
 	width: 20%;
+}
+.flex{
+	display: flex;
+}
+
+button{
+	margin-left: 5px;
+	margin-right: 5px;
+	border: 1px solid black;
+}
+
+#results{
+display: table;
+width: 100%;
+margin-top: 30px;
 }
 </style>
