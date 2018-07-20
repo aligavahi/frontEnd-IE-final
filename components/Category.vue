a0ffb5f81332c93840256033e374faaa930e3f8e
Ali Gavahi <aligavahi@yahoo.com>
2018-07-20 08:41:50 +0430

    add nuxt-link to category component

--- components/Category.vue
+++ components/Category.vue
@@ -1,25 +1,48 @@
 <template>
-  <div id= "cont" :style="'background-image: '+src">
-  <div id = "title">
-  {{title}}
-  </div>
+<div id= "cont" :style="'background-image: url('+src+')'" @mouseover="emph()" @mouseout="norm()" >
+  <nuxt-link :to="{path : link}">
+      <div id = "title" :style="'font-size: ' + size + ';background-color: ' + color" >
+      {{title}}
+      </div>
+  </nuxt-link>
 </div>
 </template>
 
 <script >
 
 export default {
-  props:["src","title"]
+  props:["src","title","link"],
+  methods: {
+      norm(){
+        this.size = "1em";
+        this.color = "rgba(100,100,100,.5)";
+      },
+
+      emph(){
+        this.size = "1.5em";
+        this.color = "rgba(200,200,200,.5)";
+      }
+  },
+   data() {
+    return {
+      size:"1em",
+      color:"rgba(100,100,100,.5)"
+    }
+  }
 }
 </script>
 
 
 
 <style scoped>
-#cont{
+
+#cont {
   padding: 5%;
   margin: 3px;
   background-color: #888888;
+  background-repeat: no-repeat;
+  background-size: 100%;
+  background-position: center;
   width: 100%;
   position:relative;
 }
@@ -28,9 +51,9 @@ export default {
   position:absolute;left:50%;bottom:0;
   transform:translate(-50%,0%);
   -ms-transform:translate(-50%,0%);
-  background-color: rgba(100,100,100,.5);
   width: 100%;
   text-align: center;
+  color: white;
 }
 
 </style>
